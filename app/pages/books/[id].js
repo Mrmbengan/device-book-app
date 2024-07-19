cb7bdfc321bf8c13f6ae84831d79b8b089b26092
Matias Bengtsson <bengtsson1985@hotmail.com>
2024-07-19 17:51:21 +0200

    implementing read status/rate status

--- app/pages/books/[id].js
+++ app/pages/books/[id].js
@@ -3,13 +3,15 @@
 
 import { useRouter } from 'next/router';
 import { useState, useEffect } from 'react';
-import { fetchBookById } from '../../utils/api';
+import { fetchBookById, rateBook, updateShelf } from '../../utils/api';
 import BookDetail from '../../components/BookDetail';
 
 const BookPage = () => {
     const router = useRouter();
     const { id } = router.query;
     const [book, setBook] = useState(null);
+    const [rating, setRating] = useState(0);
+    const [shelf, setShelf] = useState('');
 
     useEffect(() => {
         if (id) {
@@ -21,9 +23,53 @@ const BookPage = () => {
         }
     }, [id]);
 
+    const handleRatingChange = async () => {
+        try {
+            await rateBook(id, rating);
+        } catch (error) { 
+            console.error('error rating book:', error);
+        }
+    }
+
+    const handleShelfChange = async (status) => {
+        try {
+            setShelfStatus(status);
+            await updateShelf(id, status);
+        } catch (error) {
+            console.error('error updating shelf:', error);
+        }
+    }
+
     if (!book) return <div>Loading...</div>;
 
-    return <BookDetail book={book} />;
+    return (
+        <div className="container mx-auto p-4">
+            <BookDetail book={book} />;
+            <div className='mt-4'>
+                <label htmlFor='rating'>Rating</label>
+                <input
+                    id='rating'
+                    type='number'
+                    value={rating}
+                    onChange={(e) => setRating(Number(e.target.value))}
+                    min='1'
+                    max='5'
+                    onBlur={handleRatingChange}
+                    className='border border-gray-300 p-2 rounded'
+                />
+                <button onClick={handleRatingChange} className='ml-2 p-2 bg-blue-500 text-white rounded'>Rate</button>
+            </div>
+            <div className='mt-4'>
+                <button onClick={() => handleShelfChange('currently-reading')} className='p-2 bg-blue-500 text-white rounded'>Currently Reading</button>
+                <button onClick={() => handleShelfChange('want-to-read')} className='ml-2 p-2 bg-blue-500 text-white rounded'>Want to Read</button>
+                <button onClick={() => handleShelfChange('read')} className='ml-2 p-2 bg-blue-500 text-white rounded'>Read</button>
+            </div>
+
+        
+        </div>
+
+    );
+     
 };
 
 export default BookPage;
